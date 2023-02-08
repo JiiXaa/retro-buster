bc94dfdb36c997ebd1c8d49c6d42162bfb395e26
JiiXaa <anythingfx@gmail.com>
2023-02-08 22:34:53 +0000

    Refactored db models to work with Postgresql. Also database is stored in the ElephantSQL service. Tested all features and all work well.

--- app/models/videocassettes.py
+++ app/models/videocassettes.py
@@ -1,9 +1,12 @@
+### Had the issue with the UUID type not being recognized by the database
+### I found this solution on StackOverflow: https://stackoverflow.com/questions/183042/how-can-i-use-uuids-in-sqlalchemy
+from sqlalchemy.dialects.postgresql import UUID
 from app import db
 from datetime import datetime
 import uuid
 
 class Videocassette(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = db.Column(db.String(100), nullable=False)
     director = db.Column(db.String(100), nullable=False)
     genre = db.Column(db.String(100), nullable=False)
@@ -14,7 +17,7 @@ class Videocassette(db.Model):
         return f"<Videocassette id={self.id}, title={self.title}>"
 
 class Details(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     stock = db.Column(db.Integer, nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     length = db.Column(db.Integer, nullable=False)
@@ -24,7 +27,7 @@ class Details(db.Model):
     image = db.Column(db.String(200), nullable=False)
 
     videocassette = db.relationship("Videocassette", back_populates="details")
-    videocassette_id = db.Column(db.Integer, db.ForeignKey("videocassette.id"))
+    videocassette_id = db.Column(UUID(as_uuid=True), db.ForeignKey("videocassette.id"))
 
 
     def __repr__(self):
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import env
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
